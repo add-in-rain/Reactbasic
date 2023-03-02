b69912c7ad418cb559329fa0c5abf6b3a6eef92e
add-in-rain <hih8752@naver.com>
2023-03-02 17:06:35 +0900

    day1

--- reactbasic/src/App.js
+++ reactbasic/src/App.js
@@ -1,14 +1,32 @@
 /* eslint-disable */
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  let user = 'add'
+  let [count, setCount] = useState(0);
+  let [count1, setCount1] = useState(0);
+
   return (
     <div className="App">
-      <h1></h1>
-      <h2></h2>
-      <h3></h3>
-      <h4></h4>
+      <h1>Hello, {user}!</h1>
+      <p>This is a React App</p>
+
+      <div>
+        <p>클릭시 + {count}</p>
+        <button onClick={() => {
+          setCount(count + 1)
+          }}>Click Me</button>
+      </div>
+
+      <div className="list">
+        <h4>타이틀</h4>
+        <span><button className='btn' onClick={() => {
+          setCount1(count1 + 1)
+          }}>👭</button> + {count1}</span>
+        <p>안녕하세요. 저는 아무개입니다.</p>
+      </div>
     </div>
   );
 }
