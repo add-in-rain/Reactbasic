c168b4d7dcbf3a9d422b6ee83264e573ba66acfb
add-in-rain <hih8752@naver.com>
2023-03-06 14:52:06 +0900

    day3

--- reactbasic/src/component/State.jsx
+++ reactbasic/src/component/State.jsx
@@ -1,79 +1,91 @@
 import React from 'react';
-import {useState} from 'react'
+import {useState} from 'react';
 
-function App() {
-  let user = 'add'
+function State() {
+  let user = 'yejin1';
+  // react에서 변수처럼 자료를 보관할 수 있는 state문법
   let [count, setCount] = useState(0);
-  let [count1, setCount1] = useState([0,0,0]);
-  let [count2, setCount2] = useState(0);
-  let [count3, setCount3] = useState(0);
-  // 변수에 들어있는 데이터 html애 넣기
+  let [like, setLike] = useState([0,0,0]);
+  // 변수에 들어있는 데이터 html에 넣기
   // JSX 중괄호 문법 사용
-  // 한 곳에다가 여러가지 자료를 저장하고 싶을 때 Array
-  let [title, setTitle] = useState(["DW아카데미 503호", "DW아카데미 502호", "DW아카데미 501호"]);
-  let [title2, setTitle2] = useState(["돌쇠1", "돌쇠2", "돌쇠3"]);
+  // 한 곳에다가 여러가지 자료를 저장하고 싶을 때 Array[ ]
+  let [title, setTitle] = useState(["DW아카데미 503호", "DW아카데미 501호", "DW아카데미 203호"])
   let [bgColor, setbgColor] = useState('white');
+  let [name, setName] =useState(["돌쇠1", "돌쇠2", "돌쇠3"]);
+  let [ye, setYe] = ['yejin', 'lee', 'lala'];
+
 
   let changeBg = () => {
     let newBg = bgColor == 'white'? 'red' : 'white';
     setbgColor(newBg)
   }
 
-  
 
-  // map함수가 array의 갯수만큼 return을 반복한다.
-  // 매개변수(파라미터)를 만들어주면 함수 파라미터가 array자료 안에 있는 데이터가 된다.
-  // [1,2,3].map(function(num){
-  //   return (
-  //     console.log(num)
-  //   )
-  // })
+  // map함수가 array의 갯수만큼 retrun을 반복한다.
+  // 매개변수(파라미터)를 만들어주면 함수 파라미터가 array자료 안에 있는 데이터가 된다
+  [1,2,3].map(function(num){
+    // return console.log(num)
+  })
 
   // let num = [1,2,3,4,5];
-  // let newNum = num.map((num)=> num * 3);
+  // let newNum = num.map((num)=> num + 1)
   // console.log(num)
   // console.log(newNum)
 
-  // let num = [1,2,3,4,5];
-  // let newArr = num.map(function(element){
-  //   return element * 3
-  // })
-  // console.log(newArr)
+  let num = [1,2,3,4,5];
+  let newArr = num.map(function(element){
+    return element * 3
+  })
+  console.log(newArr)
 
-  // for(let i = 0; i < num.length; i++) {
-  //   num[i] = num[i] * 3;
-  // }
-  // console.log(num)
-  
+  for(let i = 0; i < num.length; i++) {
+    num[i] = num[i] * 3;
+  }
+  console.log(num)
 
-  return (
+
+  return ( 
     <div className="App" style={{backgroundColor : bgColor}}>
       <h1>Hello, {user}!</h1>
       <p>This is a React App</p>
 
       <div>
         <p>클릭시 + {count}</p>
-        <button onClick={() => {
+        <button onClick={()=>{
           setCount(count + 1)
-          }}>Click Me</button>
+        }}>Click Me</button>
       </div>
+
+      {/* {
+        [1,2,3].map(function(){
+          return <div>안녕</div>
+        })
+      } */}
+
+
+      {/* {
+        ye.map(function(a){
+          return a.toUpperCase()
+        })
+      } */}
 
       {
         title.map(function(a, i){
           return (
             <div className="list" key={i}>
-            <h4>{title[i]}</h4>
-            <span><button className='btn' onClick={() => {
-              let copy = [...count1]
-              copy[i] = copy[i] + 1
-              setCount1(copy)
-              }}>👍</button> + {count1[i]}</span>
-            <p>안녕하세요. 저는 {title2[i]}입니다.</p>
-          </div>
+              <h4>{title[i]}</h4>
+              <span onClick={()=>{
+                let copy = [...like];
+                copy[i] = copy[i] + 1
+                setLike(copy)
+              }}>👍{like[i]}</span>
+              <p>안녕하세요. 저는 {name[i]} 입니다.</p>
+            </div>
           )
         })
       }
 
+      
       {/* <div className="list">
         <h4>{title[0]}</h4>
         <span><button className='btn' onClick={() => {
@@ -98,8 +110,8 @@ function App() {
           }}>👍</button> + {count3}</span>
         <p>안녕하세요. 저는 철수입니다.</p>
       </div> */}
-      
-      <button onClick={() => {
+
+      <button onClick={()=>{
         let copy = [...title];
         copy[0] = "뻥이야!";
         copy[1] = "속았지?";
@@ -107,23 +119,19 @@ function App() {
         console.log(title == copy)
         setTitle(copy)
       }}>글제목 변경</button>
-      <button onClick={() => {
+      <button onClick={()=>{
         let copy = [...title];
-        copy.sort();
-        setTitle(copy)
-      }}>글정렬</button>
+        copy.sort()
+        setTitle(copy);
+      }}>글 정렬</button>
 
       <button onClick={changeBg}>배경색 변경</button>
-      <button onClick={() => {
-        let copy = [...title2];
+      <button onClick={()=>{
+        let copy = [...name];
         copy = ["돌쇠", "갑분이", "말숙이"]
-        setTitle2(copy)
-      }}>이름 변경</button>
+        setName(copy)
+      }}>이름변경</button>
 
-      {/* <button onClick={() => {
-          bgColor === 'black' ? 
-          setbgColor('white') : setbgColor('black')
-      }}>배경색 변경</button> */}
     </div>
   );
 }
