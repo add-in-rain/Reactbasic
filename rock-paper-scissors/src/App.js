6dcf1ebbb5126ced1eb1e01b9d1947b65843a0ca
add-in-rain <hih8752@naver.com>
2023-03-20 17:23:56 +0900

    day6

--- rock-paper-scissors/src/App.js
+++ rock-paper-scissors/src/App.js
@@ -1,14 +1,95 @@
 import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 import Box from './component/Box'
 
+// 객체는 key : value 형태로 작성한다.
+// value로 여러개의 프로퍼티를 가질 수 있다.
+// 프로퍼티 : key : value 쌍
+const choice = {
+  scissors : {
+    name : "Scissors",
+    img : "https://cdn.images.fecom-media.com/FE00015872/images/HE1815181_1426639-GLS-STA-P01.jpg"
+  },
+  rock : {
+    name : "Rock",
+    img : "https://nationaltoday.com/wp-content/uploads/2021/08/National-Pet-Rock-Day.jpg"
+  },
+  paper : {
+    name : "Paper",
+    img : "https://lp2m.uma.ac.id/wp-content/uploads/2021/02/A4-Paper.jpg"
+  }
+};
+
 function App() {
+  const [userSelect, setUserSelect] = useState(null);
+  const [computerSelect, setComputerSelect] = useState(null);
+  const [result, setResult] = useState("")
+
+  const play = (userChoice) => {
+    console.log(userChoice, "선택됨!")
+    setUserSelect(choice[userChoice])
+
+    // randomChoice()함수를 실행했을 때 결과값
+    let computerChoice = randomChoice();
+    setComputerSelect(computerChoice)
+
+    // 1. 유저가 선택한 값
+    // 2. 컴퓨터가 선택한 값
+    // 두 값을 judgment함수에 매개변수로 보내준다.
+    setResult(judgment(choice[userChoice], computerChoice))
+  }
+
+  const judgment= (user, computer)=> {
+    // 게임 로직에 대해서 생각 해보기.
+    // 어떻게 user, computer 두 값을 비교할건지?
+    // user.name == computer.name : tie
+    // user.name == "rock", computer.name == "paper": user LOSE / computer WIN
+    // user.name == "rock", computer.name == "scissors": user WIN / computer LOSE
+    // user.name == "scissors", computer.name == "paper": user WIN / computer LOSE
+    // user.name == "scissors", computer.name == "rock": user LOSE / computer WIN
+    // user.name == "paper", computer.name == "rock": user WIN / computer LOSE
+    // user.name == "paper", computer.name == "scissors": user LOSE / computer WIN
+    if(user.name == computer.name) {
+      return "tie"
+    } else if (user.name == "rock") return computer.name == "paper" ? "LOSE" : "WIN"
+      else if (user.name == "scissors") return computer.name == "paper" ? "WIN" : "LOSE"
+      else if (user.name == "paper") return computer.name == "rock" ? "WIN" : "LOSE"
+  }
+
+  const randomChoice= ()=> {
+    // 랜덤에서 우리가 가져올 수 있는 값은 숫자
+    // 이 값을 데이터랑 어떻게 연결시킬지 고민
+    // 배열의 인덱스 번호를 랜덤으로 선택되게 구현
+
+    // 객체에서 인덱스를 번호를 사용하려면 Array로 만들어야 한다.
+    // 객체의 key값을 뽑아서 Array로 만들어주는 함수 Object.keys()
+    let itemArray = Object.keys(choice);
+    console.log(itemArray)
+
+    // 0부터 1사이에 있는 랜덤한 값을 반환한다.
+    // Math.floor 소수점 아래를 버리는 함수
+    let randomItem = Math.floor(Math.random() * itemArray.length)
+    console.log(randomItem)
+
+    let final = itemArray[randomItem];
+    return choice[final]
+  }
   return (
     <div className="App">
-      <div className='box-list'>
-        <Box title="You"/>
-        <Box title="Computer"/>
-      </div>     
+      <div className="box-list">
+        <Box title="You" item={userSelect} result={result}/>
+        <Box title="Computer" item={computerSelect} result={result}/>
+      </div>
+      <div className='btn-list'>
+        {/* play함수한테 매개변수로 값을 전달한다
+            리액트에서는 play() UI를 그려줄 때 해당 함수를 실행 시킨다.
+            onClick시 실행되게 하고 싶다면 콜백함수 형태로 전달해줘야 한다.
+        */}
+        <button onClick={()=> play("scissors")}>가위</button>
+        <button onClick={()=> play("rock")}>바위</button>
+        <button onClick={()=> play("paper")}>보</button>
+      </div>
     </div>
   );
 }
--- rock-paper-scissors/src/component/Box.jsx
+++ rock-paper-scissors/src/component/Box.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
 
 const Box = (props) => {
+  let result;
+  if (
+    props.title === "computer" &&
+    props.result !== "tie" &&
+    props.result !== ""
+  ) {
+    result = props.result === "win" ? "lose" : "win"
+  } else {
+    
+  }
+
+
+  console.log(props)
   return (
     <div className='box'>
       <h1>{props.title}</h1>
-      <h2>Rock</h2>
-      <img src='https://nationaltoday.com/wp-content/uploads/2021/08/National-Pet-Rock-Day.jpg'></img>
-      <h2>win</h2>
+      <h2>{props.item && props.item.name}</h2>
+      {/* 해결 방법 : 조건부 렌더링 
+          유저가 버튼을 클릭했을 때 useState값이 들어오고,
+          값이 변경되면서 item이 img를 그려야 하는데,
+          초기값이 null이라 img를 보여주지 못하고 에러 발생!
+          => 조건부 렌더링으로 값이 null 아닐때만 실행되게 한다.
+          리액트 컴포넌트는 UI를 리턴할 때 
+          보여주고자 하는 값이 없다면 에러를 일으킨다.
+          item 값이 null 아니라면 우항의 값을 렌더링 한다.
+          item값이 null이라면 false를 리턴하니까 
+          우항이 실행되지 않는다.(렌더링X)
+      */}
+      <img src={props.item && props.item.img}></img>
+      <h2>{result}</h2>
     </div>
   )
 }
