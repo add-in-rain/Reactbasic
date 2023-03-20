ad4e9a1236cb234b3e0b0cfe737ccca91c83544c
add-in-rain <hih8752@naver.com>
2023-03-20 09:49:52 +0900

    day 6

--- rock-paper-scissors/src/App.js
+++ rock-paper-scissors/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Box from './component/Box'
 
 function App() {
   return (
     <div className="App">
-
+      <Box/>
+      <Box/>
     </div>
   );
 }
---
+++ rock-paper-scissors/src/component/Box.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Box = () => {
+  return (
+    <div className='box'>
+      <h1>User</h1>
+      <h2>Rock</h2>
+      <img src='https://nationaltoday.com/wp-content/uploads/2021/08/National-Pet-Rock-Day.jpg'></img>
+      <h2>win</h2>
+    </div>
+  )
+}
+
+export default Box
