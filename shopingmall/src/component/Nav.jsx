af21d9169c38a6ae6991765bfed5534febd4e915
add-in-rain <hih8752@naver.com>
2023-03-22 14:27:34 +0900

    day 8

--- shopingmall/src/component/Nav.jsx
+++ shopingmall/src/component/Nav.jsx
@@ -1,29 +1,44 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass, faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import { faHeart } from '@fortawesome/free-regular-svg-icons'
 
 const Nav = () => {
-  const loginList = ['매장찾기', '고객센터', '가입하기', '로그인']
-  const menuList = ['New Realeases', 'Men', 'Women', 'Kids', 'Sale', 'SNKRS', '나이키 앱']
+  const loginList = ['매장찾기', '고객센터', '가입하기', '로그인'];
+  const menuList = ['New Releases', 'Men', 'Women', 'Kids', 'Sale', 'SNKRS', '나이키 앱']
 
   return (
     <div>
       <div className='login'>
-        <ul>
+        <ul className='login-list'>
           {
-            loginList.map((item, i)=> {
+            loginList.map((item, i) => {
               return <li className='login-item' key={i}>{item}</li>
             })
           }
         </ul>
       </div>
+
       <div className='nav'>
-        <img width={70} src="https://img.freepik.com/icones-gratis/nike_318-566094.jpg?w=360" alt="" />
+        <img width={70} src="https://companieslogo.com/img/orig/NKE-0c8add60.png?t=1632522146" alt="" />
         <ul className='gnb-list'>
-        {
-            menuList.map((item, i)=> {
-              return <li className='gnb-item' key={i}>{item}</li>
+          {
+            menuList.map((menu, i) => {
+              return <li className='gnb-item' key={i}>{menu}</li>
             })
           }
         </ul>
+
+        <div className='icon-list'>
+          <div className='search-box'>
+            <FontAwesomeIcon className='ic-search' icon={faMagnifyingGlass} />
+            <input type="text" placeholder='검색'/>
+          </div>
+          <div>
+           <FontAwesomeIcon className='icon' icon={faHeart} />
+           <FontAwesomeIcon className='icon' icon={faCartShopping} />
+          </div>
+        </div>
       </div>
     </div>
   )
--- shopingmall/src/page/ProductAll.jsx
+++ shopingmall/src/page/ProductAll.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ProductAll = () => {
   return (
-    <div>ProductAll</div>
+    <div></div>
   )
 }
 
