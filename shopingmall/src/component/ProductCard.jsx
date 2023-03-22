a60ffddbb098fc4c8d2393bdd03d201d1359fb80
add-in-rain <hih8752@naver.com>
2023-03-22 16:22:27 +0900

    day 8

---
+++ shopingmall/src/component/ProductCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+      <img src="" alt="" />
+      <div>베스트셀러</div>
+      <div>제품명</div>
+      <div>가격</div>
+      <div>신제품</div>
+    </div>
+  )
+}
+
+export default ProductCard
--- shopingmall/src/page/ProductAll.jsx
+++ shopingmall/src/page/ProductAll.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {useEffect} from 'react'
+import ProductCard from '../component/ProductCard';
 
 const ProductAll = () => {
+  const [productList, setproductList] = useState([]);
+
+  const getProducts = async () => {
+    // 모든 데이터를 가진 주소를 부른다.
+    let url = "http://localhost:3004/products"
+    // url fetch 해줘라
+    let response = await fetch(url);
+    // response에서 json을 뽑아와라
+    let data = await response.json();
+    console.log(data)
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, []);
+
   return (
     <div></div>
   )
